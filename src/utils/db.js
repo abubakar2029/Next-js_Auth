acf122f7de4c93fb89d882c2d786192709110afc
Abu Bakar <abubakarzafar2029@gmail.com>
2023-11-08 01:04:59 +0500

    Data successfully saving in db

--- src/utils/db.js
+++ src/utils/db.js
@@ -2,9 +2,11 @@ import mongoose from "mongoose";
 
 const connect = async () => {
   if (mongoose.connections[0].readyState) return;
+  console.log("Hello");
+  console.log("Abc",process.env.MONGO_URL);
   try {
-    // await mongoose.connect(process.env.MONGO_URL);
-    await mongoose.connect("mongodb+srv://abubakar:Blockchain29@cluster0.kwwt0up.mongodb.net/?retryWrites=true&w=majority");
+    await mongoose.connect(process.env.MONGO_URL);
+    // await mongoose.connect("mongodb+srv://abubakar:Blockchain29@cluster0.kwwt0up.mongodb.net/?retryWrites=true&w=majority;");
     console.log("Mongoose Connection Done");
   } catch (err) {
     throw new Error("Error Connecting to Mongoose");
